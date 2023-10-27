a8ec711d3b0564f18fe2e5871bc44c2d97ea33e7
Alexander Shevelev <alex.shevelev@hotmail.com>
2023-10-27 22:33:33 +0300

    train script reads yaml config

--- train.py
+++ train.py
@@ -5,39 +5,33 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torchvision
+import yaml
 
 from utils import set_all_seeds, set_deterministic
 from dataloader import get_dataloaders_celeba
 from dcgan import DCGAN
 
 
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+with open("config.yml", 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+DEVICE = torch.device(config["device"])
 print('Device:', DEVICE)
 
-RANDOM_SEED = 42
-GENERATOR_LEARNING_RATE = 0.0002
-DISCRIMINATOR_LEARNING_RATE = 0.0002
-
-NUM_EPOCHS = 10
-BATCH_SIZE = 128
-
-IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 64, 64, 3
-
-
 set_deterministic()
-set_all_seeds(RANDOM_SEED)
+set_all_seeds(config["random_seed"])
 
 
 custom_transforms = torchvision.transforms.Compose([
     torchvision.transforms.CenterCrop((160, 160)),
-    torchvision.transforms.Resize([IMAGE_HEIGHT, IMAGE_WIDTH]),
+    torchvision.transforms.Resize([config["image_size"], config["image_size"]]),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 train_loader, val_loader, test_loader = get_dataloaders_celeba(
-    dataroot=Path("/home/alex/projects/generative_models/data/celeba"),
-    batch_size=BATCH_SIZE,
+    dataroot=Path(config["data"]),
+    batch_size=config["batch_size"],
     num_workers=10,
     train_transforms=custom_transforms,
     test_transforms=custom_transforms)
@@ -47,28 +41,21 @@ model.to(DEVICE)
 
 optim_gen = torch.optim.Adam(model.generator.parameters(),
                              betas=(0.5, 0.999),
-                             lr=GENERATOR_LEARNING_RATE)
+                             lr=config["generator_lr"])
 
 optim_discr = torch.optim.Adam(model.discriminator.parameters(),
                                betas=(0.5, 0.999),
-                               lr=DISCRIMINATOR_LEARNING_RATE)
-
-
-log_dict = {'train_generator_loss_per_batch': [],
-            'train_discriminator_loss_per_batch': [],
-            'train_discriminator_real_acc_per_batch': [],
-            'train_discriminator_fake_acc_per_batch': [],
-            'images_from_noise_per_epoch': []}
+                               lr=config["discriminator_lr"])
 
 loss_fn = F.binary_cross_entropy_with_logits
 
-latent_dim = 100
+latent_dim = config["latent_dim"]
 fixed_noise = torch.randn(64, latent_dim, 1, 1, device=DEVICE)
-logging_interval = 100
+logging_interval = config["logging_interval"]
 
 start_time = time.time()
 
-for epoch in range(NUM_EPOCHS):
+for epoch in range(config["num_epochs"]):
     model.train()
     for batch_idx, features in enumerate(train_loader):
         batch_size = features.size(0)
@@ -105,6 +92,6 @@ for epoch in range(NUM_EPOCHS):
 
         if not batch_idx % logging_interval:
             print('Epoch: %03d/%03d | Batch %03d/%03d | Gen/Dis Loss: %.4f/%.4f'
-                       % (epoch+1, NUM_EPOCHS, batch_idx, len(train_loader), gener_loss.item(), discr_loss.item()))
+                       % (epoch+1, config["num_epochs"], batch_idx, len(train_loader), gener_loss.item(), discr_loss.item()))
 
 torch.save(model.state_dict(), "gan_celeba_01.pt")
